de4c08554938372a81367d051ca9f2dc27d6ff07
Guilherme <guilhermetprado@gmail.com>
2026-03-24 01:08:44 -0300

    Repository criado

---
+++ departamentos/src/main/java/com/zera/departamentos/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.zera.departamentos.repositories;
+
+import com.zera.departamentos.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}
