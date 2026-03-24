ffacf1e63c0581203efae05ddcd1b893e74deae3
Guilherme <guilhermetprado@gmail.com>
2026-03-24 01:04:20 -0300

    Model criado

---
+++ departamentos/src/main/java/com/zera/departamentos/models/DepartamentoModel.java
@@ -0,0 +1,45 @@
+package com.zera.departamentos.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String localizacao;
+
+    public DepartamentoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
---
+++ departamentos/src/test/java/com/zera/departamentos/DepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zera.departamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
